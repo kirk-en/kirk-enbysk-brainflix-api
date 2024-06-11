2e7e2c4cb37b9d83e2519e80b2c9206c375511bd
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-10 22:54:18 -0400

    add static video image files to public directory

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import fs from "fs";
 import express from "express";
+import videoRoutes from './routes/video.js';
 
 const app = express();
+app.use('/videos', videoRoutes)
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log('middleware inovcation',  new Date().toLocaleTimeString());
+  next();
+});
 
 app.listen(8080, () => {
   console.log('Server running on port 8080');

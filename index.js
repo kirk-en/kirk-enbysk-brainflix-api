72ab8e6131abe981fa8c3559cff7f85b4ef0f992
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-10 23:45:15 -0400

    begin mapping function to return array of all video objects with limited info (for populating next videos sidebar)

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import fs from "fs";
 import express from "express";
-import videoRoutes from './routes/video.js';
+import videoRoutes from './routes/videos.js';
 
 const app = express();
 app.use('/videos', videoRoutes)
 app.use(express.static('public'));
 
+// Middleware
 app.use((req, res, next) => {
   console.log('middleware inovcation',  new Date().toLocaleTimeString());
   next();
 });
+
+app.use('/videos', videoRoutes);
 
 app.listen(8080, () => {
   console.log('Server running on port 8080');
---
+++ routes/videos.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-restricted-syntax */
+import express from 'express';
+import fs from 'fs';
+
+const router = express.Router();
+// app.use(express.json());
+
+router.get('/', (req, res) => {
+  console.log('GET videos endpoint hit!');
+  const videoData = JSON.parse(fs.readFileSync('./data/videos.json'));
+  // Map the full dataset to return a array of all videos with reduced info on each video
+  const allVideos = videoData.map((video) => {
+    const outputObj = {};
+    for (const prop in video) {
+      if (['id', 'title', 'channel', 'image'].includes(prop)) {
+        outputObj[prop] = video[prop];
+      }
+    }
+    return outputObj;
+  });
+  res.status(200).json(allVideos);
+});
+
+export default router;
